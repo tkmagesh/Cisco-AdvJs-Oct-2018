13bd0eb89a91d8566537c2a95260ffad3893ab26
Magesh <tkmagesh77@gmail.com>
2018-10-08 12:15:44 +0530

    calculator - before refactoring

--- 01-Language/src/calculator.js
+++ 01-Language/src/calculator.js
@@ -1,3 +1,16 @@
 function add(x,y){
-	return parseInt(x) + parseInt(y);
+	function parseArg(n){
+		if (Array.isArray(n)){
+			var result = 0;
+			for(var index = 0, count = n.length; index < count; index++)
+				result = result + parseArg(n[index]);
+			return result;
+		}
+		if (typeof n === 'function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n);
+	}
+	var result = 0;
+	for(var index = 0, count = arguments.length; index < count; index++)
+		result = result + parseArg(arguments[index]);
+	return result;
 }
